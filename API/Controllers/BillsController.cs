f68e4915e0fd09fa0a3c3753624831cda53dece2
Nilso Jose Baseggio Junior <nilsojr@gmail.com>
2022-07-11 22:43:01 -0300

    Created API Tests project

--- API/Controllers/BillsController.cs
+++ API/Controllers/BillsController.cs
@@ -1,8 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
 using API.DTOs;
 using API.Entities;
 using API.Extensions;
@@ -11,7 +8,6 @@ using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {

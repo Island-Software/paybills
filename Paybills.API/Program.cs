972851e7d5ff7f1e3b0b6a346c9ab858bb126954
Nilso Jose Baseggio Junior <nilsojr@gmail.com>
2022-08-13 13:05:40 -0300

    Added basic logs to app

--- Paybills.API/Program.cs
+++ Paybills.API/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Paybills.API.Data;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Paybills.API
 {
@@ -13,6 +16,8 @@ namespace Paybills.API
     {
         public static async Task Main(string[] args)
         {
+            ConfigureLogging();
+
             var host = CreateHostBuilder(args).Build();
 
             using (var scope = host.Services.CreateScope())
@@ -33,12 +38,38 @@ namespace Paybills.API
             await host.RunAsync();
         }
 
+        private static void ConfigureLogging() {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
+
+        private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"centralizador-logs-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
+
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     // webBuilder.UseUrls("https://localhost:5001");
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog();
     }
 }

1890e420adbdfd4079d0c3d4dfb3e061f4f75b8b
Nilso José Baseggio Junior <nilsojr@gmail.com>
2023-11-03 16:49:10 -0300

    Release candidate (#7)
    * Changes for release
    
    - Disabled launch browser on develop mode
    - Disabled logging
    - Updated CORS policy
    - Added client to project
    - Disabled custom logging
    - Fixed bills copy function

--- Paybills.API/Startup.cs
+++ Paybills.API/Startup.cs
@@ -39,7 +39,7 @@ namespace Paybills.API
 
             app.UseRouting();
 
-            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200", "http://localhost:4200"));
 
             app.UseAuthentication();
             app.UseAuthorization();
